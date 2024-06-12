7a2ef85da5abb51403ee19c330edeb121756633e
massonix <ramonmassoni@gmail.com>
2024-06-12 14:46:21 +0200

    update

--- scripts/2-download_fastq_files.sh
+++ scripts/2-download_fastq_files.sh
@@ -1,5 +1,5 @@
 # This script downloads all fastqs for a given gem_id (Gel-Bead-in-Emulsion)
 # first argument ($1) needs to be the actual gem_id
 
-mkdir -p $1/fastqs
-grep $1 fastq_list_tonsil_atlas_scRNA.tsv | cut -f8 | xargs -n 1 -P 4 -I {} wget -P $1/fastqs {}
+mkdir -p data/fastq/$1
+grep $1 data/fastq_list_tonsil_atlas_scRNA.tsv | cut -f8 | xargs -n 1 -P 4 -I {} wget -P data/fastq/$1 {}

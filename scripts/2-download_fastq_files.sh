650a17632b0f0f7fc307532b3188b2c1cb848fee
massonix <ramonmassoni@gmail.com>
2024-06-12 15:04:54 +0200

    type

--- scripts/2-download_fastq_files.sh
+++ scripts/2-download_fastq_files.sh
@@ -2,4 +2,4 @@
 # first argument ($1) needs to be the actual gem_id
 
 mkdir -p data/fastq/$1
-grep $1 data/fastq_list_tonsil_atlas_scRNA.tsv | cut -f8 | | tr ';' '\n' | xargs -n 1 -P 4 -I {} wget -P data/fastq/$1 {}
+grep $1 data/fastq_list_tonsil_atlas_scRNA.tsv | cut -f8 | tr ';' '\n' | xargs -n 1 -P 4 -I {} wget -P data/fastq/$1 {}
